a1d68eed79a3432a0fc254cf336cd3d0dbbc240c
Boda <bodaa2300@gmail.com>
2023-06-15 11:45:24 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,10 @@
 
 int main(void)
 {
-	printf("size of a char: %u byte(S)\n", sizeof(char));
-	printf("size of a int: %u byte(S)\n", sizeof(int));
-	printf("size of a long int: %u byte(S)\n", sizeof(long int));
-	printf("size of a long long int: %u byte(S)\n", sizeof(long long int));
-	printf("size of a float: %u byte(S)\n", sizeof(float));
+	printf("size of a char: %lu byte(S)\n", sizeof(char));
+	printf("size of a int: %lu byte(S)\n", sizeof(int));
+	printf("size of a long int: %lu byte(S)\n", sizeof(long int));
+	printf("size of a long long int: %lu byte(S)\n", sizeof(long long int));
+	printf("size of a float: %lu byte(S)\n", sizeof(float));
 	return (0);
 }
