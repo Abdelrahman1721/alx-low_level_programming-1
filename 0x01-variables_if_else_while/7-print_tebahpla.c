dd9bebf2b2f382357a4503527b1d467451fb842d
Boda <bodaa2300@gmail.com>
2023-06-16 11:12:06 -0700

    a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print reverse alphabet
+ *
+ * Return: Alawys 0 (success)
+*/
+
+
+int main(void)
+{
+	char c = 'z';
+		while (c >= 'a')
+		{
+			putchar(c);
+			c--;
+
+		}
+		putchar('\n');
+		return (0);
+
+
+
+
+
+}
