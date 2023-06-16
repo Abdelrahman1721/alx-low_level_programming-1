c443a05a3cfe30f10d631f1c451bcd09cf62ba1e
Boda <bodaa2300@gmail.com>
2023-06-16 11:34:57 -0700

    a

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: prints all the numbers of base 16 in lowercase
+ *
+ * Return: Alawys 0 (success)
+*/
+
+int main(void)
+{
+int i = 0;
+while (i < 16)
+{
+if (i < 10)
+{
+	putchar(i + '0');
+	i++;
+}
+else
+{
+	putchar(i - 10 + 'a');
+	i++;
+}
+} puthcar('\n');
+return (0);
+}
