836be7d0a71b937ab291a61fecaccb49d48d3783
Boda <bodaa2300@gmail.com>
2023-06-29 12:17:49 -0700

    a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strcat - function that concatenates
- *      two strings.
+ * _strncat - a function that concatenates two strings.
  *
- *@dest: pointer to destnation input
- *@src: pointer to source input
- *@n: most number fo byte
+ * @dest: pointer to destination input
+ * @src: pointer to source input
+ * @n: most number fo byte from @src
+ *
  * Return: @dest
 */
 
