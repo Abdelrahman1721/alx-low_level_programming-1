acdc4f2f4b64f8f4167fd5be3c72e6014d913651
Boda <bodaa2300@gmail.com>
2023-06-29 19:42:55 -0700

    a

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -36,24 +36,21 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	sum += n2[j] - '0';
 		j--;
 	}
-	if (k < size_r)
-	{
-		r[k] = sum % 10 + '0';
-		k++;
-	}
-	carry = sum / 10;
-	}
-	if (k < size_r)
-		r[k] = '\0';
+if (k < size_r)
+{
+r[k] = sum % 10 + '0';
+k++;
+}
+carry = sum / 10;
+}
+if (k < size_r)
+r[k] = '\0';
 	else
 	return (0);
-
 for (i = 0, j = k - 1; i < j; i++, j--)
-{
-char temp = r[i];
+{	char temp = r[i];
 
-r[i] = r[j];
-r[j] = temp;
-}
-return (r);
-}
+	r[i] = r[j];
+	r[j] = temp;	}
+	return (r);
+	}
