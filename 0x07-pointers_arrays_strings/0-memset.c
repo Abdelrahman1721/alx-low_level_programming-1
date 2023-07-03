0df159899d69c7fcce9ce6bc4045c54bafc22231
Boda <bodaa2300@gmail.com>
2023-07-03 12:28:35 -0700

    a

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,19 @@
 #include"main.h"
-
 /**
- * *_memset - prints array.
- * @s: pointer
- * @b: constant
+ * _memset - prints array.
+ * @s: char
+ * @b: char
  * @n: integer
  *
- * Return: pointer s
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-
 char *_memset(char *s, char b, unsigned int n)
-{	
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-	s[i] = b;
-	}
-	return (s);
+{
+unsigned int i;
+for (i = 0; i < n ; i++)
+{
+s[i] = b;
+}
+return (s);
 }
