f136bfc75bb85a7ef3e5d48537c3a6bb695643be
Boda <bodaa2300@gmail.com>
2023-07-03 12:50:45 -0700

    a

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strstr - Locates a substring
+ *
+ * @haystack: char string
+ * @needle: char string
+ *
+ * Return: char pointer or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j;
+
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (needle[i] == haystack[j])
+			{
+				return (haystack + j);
+			}
+		}
+	}
+	return ('\0');
+}
