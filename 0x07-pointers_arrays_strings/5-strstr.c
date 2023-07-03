4ce3eb4f509111fdd60d3128d960b823b46a2df7
Boda <bodaa2300@gmail.com>
2023-07-03 12:58:34 -0700

    a

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,13 +13,13 @@ char *_strstr(char *haystack, char *needle)
 	int i;
 	int j;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; haystack[j] != '\0'; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (needle[i] == haystack[j])
+			if (haystack[i] == needle[j])
 			{
-				return (&haystack[j]);
+				return (&needle[j]);
 			}
 		}
 	}
