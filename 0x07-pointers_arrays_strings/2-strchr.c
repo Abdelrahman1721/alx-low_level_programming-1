b0ed97885483ace60b9bdf721e64b55c62481cee
Boda <bodaa2300@gmail.com>
2023-07-03 12:30:39 -0700

    a

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,8 +7,10 @@
  * Return: s
 */
 char *_strchr(char *s, char c)
-{
-	for (unsigned int i = 0; s[i] != '\0'; i++)
+{	
+	unsigned int i;	
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
