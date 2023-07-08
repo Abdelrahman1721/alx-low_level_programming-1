915436c96c4fc4f2c1406368bb7629c6a27d5c04
Boda <bodaa2300@gmail.com>
2023-07-07 18:35:09 -0700

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,8 +20,7 @@ int main(int argc, char *argv[])
 		for (c = argv[argc]; *c; c++)
 		{
 			if (*c < '0' || *c > '9')
-				printf("ERROR\n");
-					return (1);
+					return (printf("Error\n"), 1);
 			add += atoi(argv[i]);
 		}
 	}
