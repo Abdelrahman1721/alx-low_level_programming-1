36bfadde1dc45e0534c8ab3e8990cc09f38c1a1a
Boda <bodaa2300@gmail.com>
2023-07-11 12:26:47 -0700

    a

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * alloc_grid - nested loop to make grid
+ * @width: width input
+ * @height: height input
+ * Return: pointer to 2 dim. array
+ */
+
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, **bat;
+
+	bat = malloc(sizeof(**bat) * height);
+	if (width <= 0 || height <= 0 || bat <= 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+	for (i = 0; i < height; i++)
+	{
+		bat[i] = malloc(sizeof(**bat) * width);
+		if (bat[i] == 0)
+		{
+			while (i--)
+				free(bat[i]);
+			free(bat);
+			return (NULL);
+		}
+	for (j = 0; j < width; j++)
+		bat[i][j] = 0;
+	}
+	}
+	return (bat);
+}
+
