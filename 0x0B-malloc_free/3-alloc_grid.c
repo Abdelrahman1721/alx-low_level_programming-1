e7e8daf15a518c01040c0dc9ba487c3636c12e59
Boda <bodaa2300@gmail.com>
2023-07-11 12:35:43 -0700

    a

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 	{
 		for (i = 0; i < height; i++)
 		{
-			bat[i] = malloc(sizeof(*bat) * width);
+			bat[i] = malloc(sizeof(**bat) * width);
 			if (bat[i] == 0)
 			{
 				while (i--)
